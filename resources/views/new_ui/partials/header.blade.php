944c404b3db70f2484a679efba9d3191df477c0e
wulansftrri <wulansafitri2567@gmail.com>
2022-05-21 10:57:05 +0700

    'update'

--- resources/views/new_ui/partials/header.blade.php
+++ resources/views/new_ui/partials/header.blade.php
@@ -2,9 +2,9 @@
     <div class="row mx-auto" style="margin-top: 100px">
         <div class="col align-self-center">
             <p class="judul">PROGRAM STUDI TEKNIK INDUSTRI</p>
-            <P class="judul">FAKULTAS ILMU KOMPUTER</P>
+            <P class="judul">FAKULTAS TEKNIK</P>
             <P class="judul">UNIVERSITAS BINA DARMA</P>
-            <img src={{ asset('assets/images/TeknikIndustri.png') }} alt="Teknik Industri Akreditasi B"
+            <img src={{ asset('assets/images/TIN.png') }} alt="Teknik Industri Akreditasi B"
                 style="width: 200px" class="text-end">
         </div>
         <div class="col text-end">
